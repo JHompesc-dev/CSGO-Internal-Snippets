42ed16d0de2d84b27912fd0bbd1ff3430c85e2ba
JHompesc-dev <jhompesc@avans.nl>
2020-02-23 23:38:49 +0100

    Create crosshair-hitmarker.cpp

---
+++ crosshair-hitmarker.cpp
@@ -0,0 +1,11 @@
+interfaces.surface->drawLine(CrosshairX, CrosshairY, CrosshairX, CrosshairY - 6);
+interfaces.surface->drawLine(CrosshairX, CrosshairY - 6, CrosshairX + 6, CrosshairY - 6);
+
+interfaces.surface->drawLine(CrosshairX, CrosshairY, CrosshairX + 6, CrosshairY);
+interfaces.surface->drawLine(CrosshairX + 6, CrosshairY, CrosshairX + 6, CrosshairY + 6);
+
+interfaces.surface->drawLine(CrosshairX, CrosshairY, CrosshairX, CrosshairY + 6);
+interfaces.surface->drawLine(CrosshairX, CrosshairY + 6, CrosshairX - 6, CrosshairY + 6);
+
+interfaces.surface->drawLine(CrosshairX, CrosshairY, CrosshairX - 6, CrosshairY);
+interfaces.surface->drawLine(CrosshairX - 6, CrosshairY, CrosshairX - 6, CrosshairY - 6);
